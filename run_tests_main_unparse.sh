43b546bc8851c77a5faa489f288f63395f1ddbbb
Akhil Akkapelli <41839847+AkhilAkkapelli@users.noreply.github.com>
2021-12-18 12:32:54 +0530

    Delete run_tests_main_unparse.sh

--- run_tests_main_unparse.sh
+++
@@ -1,6 +0,0 @@
-inputfortranpath="./SymbolTableGen/InputFortranPrograms/"
-outputgopath="./SymbolTableGen/OutputGoPrograms/"
-outputtexpath="./SymbolTableGen/STOutputTexCodes/"
-outputpdfpath="./SymbolTableGen/STOutputPdfFiles/"
-
-python main_unparse.py $inputfortranpath $outputgopath $outputtexpath $outputpdfpath
